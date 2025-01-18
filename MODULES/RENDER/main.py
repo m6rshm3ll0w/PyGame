b67201a7c89418680b2deaa6e04276087b4db85c
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2025-01-18 22:18:12 +0300

    add movement with w,a,s,d group_sprite_clearing for better performance fix small bugs
    by @m6rshm3ll0w

--- MODULES/RENDER/main.py
+++ MODULES/RENDER/main.py
@@ -9,6 +9,7 @@ FPS = int(CONFIG["pygame"]["FPS"])
 
 
 def main(screen, size):
+
     clock = pg.time.Clock()
     game_surf = pygame.surface.Surface((int(size[0]), int(size[1])))
     gui_surf = pygame.surface.Surface((int(size[0]), int(size[1])))
@@ -26,17 +27,29 @@ def main(screen, size):
         screen.fill(BLACK)
         game_surf.fill(BLACK)
         gui_surf.fill(BLACK)
+        World.groups_clear()
 
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_w:
+                    World.center_point(0, 1, y_minus=True)
+                if event.key == pg.K_s:
+                    World.center_point(0, 1, y_plus=True)
+                if event.key == pg.K_a:
+                    World.center_point(1, 0, x_minus=True)
+                if event.key == pg.K_d:
+                    World.center_point(1, 0, x_plus=True)
 
         World.draw_floor()
+        World.draw_wall()
 
         FLOOR = World.sprite_list_Floor()
         FLOOR.draw(screen)
         FLOOR.update()
+
 
         WALL = World.sprite_list_Wall()
         WALL.draw(game_surf)
