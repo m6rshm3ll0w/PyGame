649ff01d8e4477acaf425fa7ab78fde20ac515bc
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2025-01-19 02:08:42 +0300

    micro bugs and structure

--- MODULES/RENDER/main.py
+++ MODULES/RENDER/main.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame as pg
 
+from MODULES.Entitys.player import Player as Player_class
 from MODULES.MAP.generate import MAP_GENERATION
 from MODULES.RENDER.render_world import WorldClass
 from MODULES.init import CONFIG, BLACK
@@ -8,56 +9,90 @@ from MODULES.init import CONFIG, BLACK
 FPS = int(CONFIG["pygame"]["FPS"])
 
 
-def main(screen, size):
+def upd_map(World, screen, game_surf):
+    World.draw_floor()
+    World.draw_wall()
 
-    clock = pg.time.Clock()
+    FLOOR = World.sprite_list_Floor()
+    FLOOR.draw(screen)
+    FLOOR.update()
+
+    WALL = World.sprite_list_Wall()
+    WALL.draw(game_surf)
+    WALL.update()
+
+
+def set_up_layers(size):
+    floor_surf = pygame.surface.Surface((int(size[0]), int(size[1])))
     game_surf = pygame.surface.Surface((int(size[0]), int(size[1])))
     gui_surf = pygame.surface.Surface((int(size[0]), int(size[1])))
-
     game_surf.set_colorkey(BLACK)
     gui_surf.set_colorkey(BLACK)
+    floor_surf.set_colorkey(BLACK)
 
-    WLD = MAP_GENERATION()
-    World = WorldClass(WLD, screen, game_surf)
+    center = game_surf.get_rect().center
 
+    return center, floor_surf, game_surf, gui_surf
+
+
+def create_obj(screen, game_surf):
+    World = WorldClass(MAP_GENERATION(), screen, game_surf)
+    Player = Player_class(x=int(game_surf.get_width() // 2), y=int(game_surf.get_height() // 2))
+
+    return World, Player
+
+
+def update_screen(screen, floor_surf, game_surf, gui_surf, clock):
+    clock.tick(int(FPS))
+    game_surf.blit(gui_surf, (0, 0))
+    floor_surf.blit(game_surf, (0, 0))
+    screen.blit(floor_surf, (0, 0))
+    pygame.display.flip()
+
+
+def clear_screen(screen, floor_surf, game_surf, gui_surf, World):
+    screen.fill(BLACK)
+    floor_surf.fill(BLACK)
+    game_surf.fill(BLACK)
+    gui_surf.fill(BLACK)
+    World.groups_clear()
+
+
+def main(screen, size):
+    pg.event.set_allowed([pg.QUIT])
+
+    clock = pg.time.Clock()
+
+    center, floor_surf, game_surf, gui_surf = set_up_layers(size)
+
+    World, Player = create_obj(floor_surf, game_surf)
     World.generate_worldmap()
 
     running = True
     while running:
-        screen.fill(BLACK)
-        game_surf.fill(BLACK)
-        gui_surf.fill(BLACK)
-        World.groups_clear()
-
+        clear_screen(screen, floor_surf, game_surf, gui_surf, World)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    World.center_point(0, 1, y_minus=True)
-                if event.key == pg.K_s:
-                    World.center_point(0, 1, y_plus=True)
-                if event.key == pg.K_a:
-                    World.center_point(1, 0, x_minus=True)
-                if event.key == pg.K_d:
-                    World.center_point(1, 0, x_plus=True)
 
-        World.draw_floor()
-        World.draw_wall()
+        keys = pygame.key.get_pressed()
 
-        FLOOR = World.sprite_list_Floor()
-        FLOOR.draw(screen)
-        FLOOR.update()
+        if (Player.x - center[0]) >= 64:
+            World.center_point(200//FPS, "-x")
+        if (Player.y - center[1]) >= 64:
+            World.center_point(200//FPS, "-y")
+        if (center[0] - Player.x) >= 64:
+            World.center_point(200//FPS, "+x")
+        if (center[1] - Player.y) >= 64:
+            World.center_point(200//FPS, "+y")
 
+        Player.move(keys, center)
 
-        WALL = World.sprite_list_Wall()
-        WALL.draw(game_surf)
-        WALL.update()
+        upd_map(World, floor_surf, game_surf)
+        Player.draw(game_surf)
 
-        clock.tick(FPS)
-        game_surf.blit(gui_surf, (0, 0))
-        screen.blit(game_surf, (0, 0))
-        pygame.display.flip()
+        update_screen(screen, floor_surf, game_surf, gui_surf, clock)
+
 
 
--- MODULES/RENDER/render_world.py
+++ MODULES/RENDER/render_world.py
@@ -36,8 +36,11 @@ class WorldClass:
         self.wall_surface = wall_surface
         self.tiles_img = None
 
-        self.x_coord = 20
-        self.y_coord = 20
+        self.x_coord = 0
+        self.y_coord = 0
+
+        self.x_centr = 0
+        self.y_centr = 0
 
         self.draw_dist = CONFIG["pygame"]["distance"]
         self.crop_tiles()
@@ -56,7 +59,9 @@ class WorldClass:
                 up_left_draw_corner[1] - (SIZE * self.draw_dist + 1))
 
     def draw_floor(self):
-        center = self.get_center_tile_corner()
+        self.check_centr()
+        center0 = self.get_center_tile_corner()
+        center = center0[0] + self.x_centr, center0[1] + self.y_centr
 
         for x_c in range(center[0], center[0] + (SIZE*self.draw_dist*2)+1, SIZE):
             for y_c in range(center[1], center[1] + (SIZE*self.draw_dist*2)+1, SIZE):
@@ -81,10 +86,13 @@ class WorldClass:
                 raise AttributeError("ээээээ я хз что это")
 
     def draw_wall(self):
-        center = self.get_center_tile_corner()
+        self.check_centr()
+        center0 = self.get_center_tile_corner()
+        center = center0[0] + self.x_centr, center0[1] + self.y_centr
         TILEMAP = self.WORLD.get_map()
 
-        render_list = TILEMAP[:]
+
+        render_list = TILEMAP.copy()
         render_list = render_list[int(self.y_coord) + self.draw_dist:][:self.draw_dist * 2 + 1]
 
         for n, row in enumerate(render_list):
@@ -94,8 +102,6 @@ class WorldClass:
         for row, x_c in enumerate(range(center[0], center[0] + (SIZE * self.draw_dist * 2) + 1, SIZE)):
             for col, y_c in enumerate(range(center[1], center[1] + (SIZE * self.draw_dist * 2) + 1, SIZE)):
                 try:
-                    if row < 0 or col < 0:
-                        raise IndexError
                     tile = self.search_tile(row, col, render_list)
                     self.draw_tile(up_left_draw_corner=(x_c, y_c), tile_type=self.tiles_img[tile], wall=True)
                 except IndexError:
@@ -106,6 +112,24 @@ class WorldClass:
         for tile, data in TILES.items():
             if data["ej"] == render_list[col][row]:
                 return tile
+
+    def check_centr(self):
+        center0 = self.get_center_tile_corner()
+        center = (center0[0] + self.x_centr), (center0[1] + self.y_centr)
+
+        if (center0[0] - center[0]) >= 32:
+            self.x_centr = 0
+            self.x_coord += 1
+        if (center[0] - center0[0]) >= 32:
+            self.x_centr = 0
+            self.x_coord -= 1
+
+        if (center0[1] - center[1]) >= 32:
+            self.y_centr = 0
+            self.y_coord += 1
+        if (center[1] - center0[1]) >= 32:
+            self.y_centr = 0
+            self.y_coord -= 1
 
     def get_world(self):
         return self.WORLD
@@ -140,21 +164,32 @@ class WorldClass:
         self.WALL = pg.sprite.Group()
         self.FLOOR = pg.sprite.Group()
 
-    def center_point(self, x, y,
-                     x_plus=False, y_plus=False,
-                     x_minus=False, y_minus=False,
-                     set_x=False, set_y=False):
-        if x_minus:
-            self.x_coord -= x
-        elif x_plus:
-            self.x_coord += x
+    # def tile_point(self, x, y,
+    #                  x_plus=False, y_plus=False,
+    #                  x_minus=False, y_minus=False,
+    #                  set_x=False, set_y=False):
+    #     if x_minus:
+    #         self.x_coord -= x
+    #     elif x_plus:
+    #         self.x_coord += x
+    #
+    #     if y_minus:
+    #         self.y_coord -= y
+    #     elif y_plus:
+    #         self.y_coord += y
+    #
+    #     if set_x:
+    #         self.x_coord = x
+    #     if set_y:
+    #         self.y_coord = y
 
-        if y_minus:
-            self.y_coord -= y
-        elif y_plus:
-            self.y_coord += y
+    def center_point(self, other:int, operat:str):
+        if operat == "+x":
+            self.x_centr += other
+        if operat == "-x":
+            self.x_centr -= other
 
-        if set_x:
-            self.x_coord = x
-        if set_y:
-            self.y_coord = y
+        if operat == "+y":
+            self.y_centr += other
+        if operat == "-y":
+            self.y_centr -= other
